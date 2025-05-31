69e97d097298a1af9eaa2ea8f32a76e087b87e52
Daniel <josedanielmedinagarcia2@gmail.com>
2025-05-31 18:04:12 -0500

    inicializando el proyecto Spring boot

---
+++ src/main/java/com/proyectoYT/FullSpring/FullSpringApplication.java
@@ -0,0 +1,13 @@
+package com.proyectoYT.FullSpring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FullSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FullSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/proyectoYT/FullSpring/FullSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.proyectoYT.FullSpring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FullSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
