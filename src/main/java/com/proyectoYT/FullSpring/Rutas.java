693bd169e7aa0dd719b0a25d6179c8a13beb9e75
Daniel <josedanielmedinagarcia2@gmail.com>
2025-06-01 21:04:09 -0500

    body params

--- src/main/java/com/proyectoYT/FullSpring/Rutas.java
+++ src/main/java/com/proyectoYT/FullSpring/Rutas.java
@@ -1,12 +1,15 @@
 package com.proyectoYT.FullSpring;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
 
 @RestController
 public class Rutas {
+
+    private final Logger logger = LoggerFactory.getLogger(Rutas.class);
 
     @GetMapping("/hola")
     String miPrimeraRuta() {
@@ -21,6 +24,15 @@ public class Rutas {
     @GetMapping("/libro2/{id}")
     String leerLibro2(@PathVariable int id, @RequestParam String params, @RequestParam String editorial) {
         return "Leyendo el libro id: " + id + ", params: " + params + ", editorial " + editorial;
+    }
+
+    @PostMapping("/libro")
+    String guardarLibro(@RequestBody Map<String, Object> libro) {
+        libro.keySet().forEach(llave -> {
+            logger.debug("llave {} valor {}", llave, libro.get(llave));
+        });
+
+        return "libro guardado";
     }
 
 }
