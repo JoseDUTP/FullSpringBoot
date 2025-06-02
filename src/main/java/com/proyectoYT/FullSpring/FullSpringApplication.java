18d5f3918296040c45d4bb83c477ee0605b78cdd
Daniel <josedanielmedinagarcia2@gmail.com>
2025-06-01 20:34:04 -0500

    query params

--- src/main/java/com/proyectoYT/FullSpring/FullSpringApplication.java
+++ src/main/java/com/proyectoYT/FullSpring/FullSpringApplication.java
@@ -1,13 +1,20 @@
 package com.proyectoYT.FullSpring;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class FullSpringApplication {
 
+	private static Logger logger = LoggerFactory.getLogger(FullSpringApplication.class);
+
 	public static void main(String[] args) {
+
 		SpringApplication.run(FullSpringApplication.class, args);
+
+		logger.debug("Mi mensaje debug");
 	}
 
 }
