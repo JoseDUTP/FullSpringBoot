990339dbffc2d6fd3debe9d133a1837fc50b843a
Daniel <josedanielmedinagarcia2@gmail.com>
2025-06-01 21:45:53 -0500

    http status II

--- src/main/java/com/proyectoYT/FullSpring/Rutas.java
+++ src/main/java/com/proyectoYT/FullSpring/Rutas.java
@@ -3,6 +3,8 @@ package com.proyectoYT.FullSpring;
 import com.proyectoYT.FullSpring.models.Libro;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Map;
@@ -32,6 +34,29 @@ public class Rutas {
         logger.debug("nombre {} editorial {}", libro.nombre, libro.editorial);
 
         return "libro guardado";
+    }
+
+    @GetMapping("/saludar")
+    @ResponseStatus(value = HttpStatus.MOVED_PERMANENTLY, reason = "Fue movida a la version 2 de la API")
+    String miSegundaRuta() {
+        return "Aprendiendo Spring";
+    }
+
+    @GetMapping("/animales/{lugar}")
+    public ResponseEntity<String> getAnimales(@PathVariable String lugar) {
+       if (lugar.equals("granja")) {
+           return ResponseEntity.status(HttpStatus.OK).body("Caballo, vaca, oveja, gallina");
+       } else if (lugar.equals("selva")) {
+           return ResponseEntity.status(HttpStatus.OK).body("mono, gorila");
+       } else {
+           return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("lugar no valido");
+       }
+
+    }
+
+    @GetMapping("/calcular/{numero}")
+    public int getCalculo(@PathVariable int numero) {
+        throw new NullPointerException("La clave del usuario es password123 y no debería leerse en postman");
     }
 
 }
